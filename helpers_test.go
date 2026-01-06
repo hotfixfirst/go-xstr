543168a2610ffcf4b3e39a53575875dd111bb88a
zunanuz <zeomatrix24@gmail.com>
2026-01-06 18:11:15 +0700

    Add stringPtr helper function for creating string pointers in tests

---
+++ helpers_test.go
@@ -0,0 +1,7 @@
+package xstr
+
+// stringPtr returns a pointer to the given string value.
+// This is a test helper for creating string pointers inline.
+func stringPtr(s string) *string {
+	return &s
+}
